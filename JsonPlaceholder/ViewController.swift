c5d6a73405deb7bb353de1a4c81a48db441b4dd0
Mustafa <mustafakilinc429@gmail.com>
2020-01-18 12:43:27 +0300

    Initial Commit

---
+++ JsonPlaceholder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JsonPlaceholder
+//
+//  Created by apple on 18.01.2020.
+//  Copyright © 2020 Mustafa KILINÇ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
